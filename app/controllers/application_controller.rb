8a3bf2c52a76d9b2e0bc7e93dec2478ee4cd2cbf
dwarfsawfish <akamaki_0729@yahoo.co.jp>
2023-02-09 16:39:56 +0900

    basic認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :user_permitted_parameters, if: :devise_controller?
   before_action :company_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   private
   def user_permitted_parameters
@@ -11,5 +12,10 @@ class ApplicationController < ActionController::Base
   def company_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:company_name, :category_id, 
       :prefecture_id, :city, :house_number, :building, :last_name, :first_name])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]    end
   end
 end
